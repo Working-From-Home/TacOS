267262c39893fc3e0b65595e6e30d5a422dad94c
whayter <66110072+whayter@users.noreply.github.com>
2025-05-17 16:21:52 +0000

    basic input

---
+++ src/io/port.rs
@@ -0,0 +1,25 @@
+/// ![Port I/O](https://en.wikipedia.org/wiki/Port_(computer_architecture))
+
+use core::arch::asm;
+
+pub fn inb(port: u16) -> u8 {
+    let mut ret: u8;
+    unsafe {
+        asm!(
+            "in al, dx",    // lit un byte du port dans dx vers al
+            out("al") ret,  // la sortie est dans al -> ret
+            in("dx") port,  // l'entrée est dans dx -> port
+        );
+    }
+    ret
+}
+
+pub fn outb(port: u16, val: u8) {
+    unsafe {
+        asm!(
+            "out dx, al",    // envoie un byte de al vers port dans dx
+            in("dx") port,
+            in("al") val,
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@
 #![no_main]
 
 mod klib;
+mod io;
+mod drivers;
 
 use core::panic::PanicInfo;
-use klib::vga;
+
+use drivers::keyboard;
+use drivers::vga;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -15,5 +19,15 @@ fn panic(_info: &PanicInfo) -> ! {
 pub extern "C" fn rust_main() -> ! {
     let msg = b"42\0";
     vga::putstr(msg.as_ptr());
-    loop {}
+    
+    loop {
+        unsafe {
+            if let Some(scancode) = keyboard::read_scancode() {
+                let ascii = keyboard::scancode_to_ascii(scancode);
+                if let Some(c) = ascii {
+                    vga::putchar(c as u8);
+                }
+            }
+        }
+    }
 }
